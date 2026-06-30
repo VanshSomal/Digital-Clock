edae26e973750dd2bfcbb7837a731b96e70827a8
VanshSomal <vanshsomal20@gmail.com>
2026-06-30 22:53:10 +0530

    feat: date added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function updateClock(){
     const clockElement=document.getElementById('clock');
+    const dateElement=document.getElementById('date');
     const now=new Date();
 
     let hours=now.getHours();
@@ -21,8 +22,25 @@ function updateClock(){
 
     // update the DOM
     clockElement.textContent=timeString;
+
+
+    // DATE LOGIC
+    // OPtions to format the date nicely
+    const options={
+        weekday:'long',
+        year:'numeric',
+        month:'long',
+        day:'numeric'
+    };
+    // Formats dae to local readable text (eg. "Tuesday. June 30, 2026")
+    const dateString=now.toLocaleDateString('en-US',options);
+    // Update the DOM for date
+    dateElement.textContent=dateString;
 }
 
 updateClock();
 // Run the function every 1 second (1000 miliseconds)
 setInterval(updateClock,1000);
+
+
+// npx @tailwindcss/cli -i ./input.css -o ./output.css --watch
